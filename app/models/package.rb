8890eacde215aff21e2a6cac6cffe2179f039053
Matt Powell <fauxparse@gmail.com>
2016-06-12 11:33:21 +1200

    make package url scoped to festival

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,7 +1,7 @@
 class Package < ApplicationRecord
   belongs_to :festival
 
-  acts_as_url :name, url_attribute: :slug, sync_url: true
+  acts_as_url :name, url_attribute: :slug, sync_url: true, scope: :festival_id
   acts_as_list scope: :festival, top_of_list: 0
 
   validates :name,
