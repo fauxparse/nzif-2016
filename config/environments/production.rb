217ea1eb463ca039520361df6e29f03a6f5ae0e0
Matt Powell <fauxparse@gmail.com>
2016-08-28 13:18:44 +1200

    adds URL config for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { host: 'nzif.info' }
 end
