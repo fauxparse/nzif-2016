ca6696129c564f9e651240bd42f21fae85292406
Matt Powell <fauxparse@gmail.com>
2016-09-07 09:30:51 +1200

    adds pricing information

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     end
     get "/activities" => "activities#index", as: :activities
     get "/code-of-conduct" => "code_of_conduct#show", as: :code_of_conduct
+    get "/pricing" => "prices#index", as: :pricing
     get "/" => "festivals#show", as: :festival
   end
 
