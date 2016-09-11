12c24fdd80f6696ef3d73294188f0fc9d578aff3
Matt Powell <fauxparse@gmail.com>
2016-09-11 16:24:31 +1200

    properly applies ActivityList sort params (fixes #38)

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,16 @@
 class ActivitiesController < ApplicationController
   def index
-    @activity_list = ActivityList.new(festival, type: params[:activity_type])
+    @activity_list = ActivityList.new(
+      festival,
+      type: params[:activity_type],
+      sort_by: :time
+    )
   end
 
   def show
     @activity_list = ActivityList.new(
       festival,
-      type: params[:activity_type],
-      sort_by: :time
+      type: params[:activity_type]
     )
     @activity = ActivityDetails.new(@activity_list.find(params[:id]), registration)
   end
