a4788f7de7a80970bfc823b02ba173f3d515c431
Matt Powell <fauxparse@gmail.com>
2016-10-01 09:00:13 +1300

    checks for nil participant (fixes #50)

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,5 +1,13 @@
 class WorkshopsController < ApplicationController
+  before_action :require_participant
+
   def index
     @rolls = WorkshopRolls.new(participant, festival).rolls
+  end
+
+  private
+
+  def require_participant
+    redirect_to festival_path(festival) unless participant.present?
   end
 end
